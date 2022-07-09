ecee6c9c1160d56ba9487bfe87abf1424f16d896
ericscott1969 <erics@eventpoint.com>
2022-07-09 16:42:31 -0700

    cleanup

--- Subfloor.Templates.Blazor/Server/Program.cs
+++ Subfloor.Templates.Blazor/Server/Program.cs
@@ -1,6 +1,7 @@
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
+using Subfloor.Dotnet;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -25,42 +26,60 @@ try
             "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
             theme: AnsiConsoleTheme.Code));
 
+    builder.Services.AddHttpClient();
+    builder.Services.AddHttpContextAccessor();
+
     builder.Services.AddControllers();
     builder.Services.AddRazorPages();
     builder.Services.AddBff();
 
-    builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = "cookie";
-        options.DefaultChallengeScheme = "oidc";
-        options.DefaultSignOutScheme = "oidc";
-    })
-        .AddCookie("cookie", options =>
-        {
-            options.Cookie.Name = "__Host-blazor";
-            options.Cookie.SameSite = SameSiteMode.Strict;
-        })
-        .AddOpenIdConnect("oidc", options =>
-        {
-            options.Authority = "https://demo.duendesoftware.com";
+    builder.Services.AddSubfloorAuthentication();
 
-            // confidential client using code flow + PKCE
-            options.ClientId = "interactive.confidential";
-            options.ClientSecret = "secret";
-            options.ResponseType = "code";
-            options.ResponseMode = "query";
+    ////this section will be replaced with a webstartupextension method (e.g. AddSubfloorAuthentication())
+    //var Configuration = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+    //var subfloorConfig = Configuration.GetSection("Subfloor");
 
-            options.MapInboundClaims = false;
-            options.GetClaimsFromUserInfoEndpoint = true;
-            options.SaveTokens = true;
+    //builder.Services.AddAuthentication(options =>
+    //{
+    //    options.DefaultScheme = "cookie";
+    //    options.DefaultChallengeScheme = "oidc";
+    //    options.DefaultSignOutScheme = "oidc";
+    //})
+    //    .AddCookie("cookie", options =>
+    //    {
+    //        options.Cookie.Name = "__Host-blazor";
+    //        options.Cookie.SameSite = SameSiteMode.Strict;
+    //    })
+    //    .AddOpenIdConnect("oidc", options =>
+    //    {
+    //        //options.Authority = "https://demo.duendesoftware.com";
 
-            // request scopes + refresh tokens
-            options.Scope.Clear();
-            options.Scope.Add("openid");
-            options.Scope.Add("profile");
-            options.Scope.Add("api");
-            options.Scope.Add("offline_access");
-        });
+    //        //// confidential client using code flow + PKCE
+    //        //options.ClientId = "interactive.confidential";
+    //        //options.ClientSecret = "secret";
+
+    //        options.Authority = subfloorConfig.GetValue<string>("Identity:Authority");
+    //        options.ClientId = subfloorConfig.GetValue<string>("Identity:Client_Id"); ;
+    //        options.ClientSecret = subfloorConfig.GetValue<string>("Identity:ClientSecret"); ;
+
+    //        options.ResponseType = "code";
+    //        options.ResponseMode = "query";
+
+    //        options.MapInboundClaims = false;
+    //        options.GetClaimsFromUserInfoEndpoint = true;
+    //        options.SaveTokens = true;
+
+    //        // request scopes + refresh tokens
+    //        options.Scope.Clear();
+    //        options.Scope.Add("openid");
+    //        options.Scope.Add("profile");
+    //        //options.Scope.Add("api");
+    //        options.Scope.Add("offline_access");
+    //    });
+
+
+    ////end auth section to be replaced by webstartupextension
+
 
     var app = builder.Build();
 
